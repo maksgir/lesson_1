e6a2bad1772b1bc738fdc633493aa444b7e5062c
maksgirya <maksgirya@mail.ru>
2020-12-03 12:16:14 +0200

    Шахматная доска

--- game.py
+++ game.py
@@ -1,10 +1,13 @@
 import pygame
-
+import random
 
 if __name__ == '__main__':
     pygame.init()
     size = width, height = 800, 600
     screen = pygame.display.set_mode(size)
+    screen.fill((0, 0, 0))
+    pygame.draw.line(screen, (100, 255, 100), (10, 10), (310, 310), width=1)
+    pygame.draw.rect(screen, (255,100,100), (10,10,300,300), width=3)
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
--- task.py
+++ task.py
@@ -2,6 +2,33 @@ import pygame
 import random
 
 
+def draw_chess_bar(s, n):
+    pygame.init()
+    size = width, height = s, s
+    if n % 2 == 0:
+        color = 'white'
+        flag = 0
+    else:
+        color = 'black'
+        flag = 1
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Шахматная клетка')
+    w = h = s // n
+    sz = s // n
+    for column in range(n):
+        for row in range(n):
+            screen.fill(color, pygame.Rect(column * sz, row * sz, sz, sz))
+            if color == 'white':
+                color = 'black'
+            else:
+                color = 'white'
+        if flag == 0:
+            if color == 'white':
+                color = 'black'
+            else:
+                color = 'white'
+
+
 def draw_kross():
     pygame.init()
     size = width, height = 800, 600
@@ -21,15 +48,22 @@ def draw_rectangle(w, h):
 
 
 if __name__ == '__main__':
-    print('Введите название задания  (Крест, Прямоугольник, Шахматная доска, Мишень)')
+    print('Введите номер задания  (Крест - 1, Прямоугольник - 2, Шахматная доска - 3, Мишень - 4)')
     which_function = input()
 
-    if which_function == 'Крест':
+    if which_function == '1':
         draw_kross()
-    if which_function == 'Прямоугольник':
+
+    if which_function == '2':
+        print('Введите размеры окна (ширина высота)')
         s = input().split()
         wi, he = s[0], s[1]
         draw_rectangle(int(wi), int(he))
+    if which_function == '3':
+        print('Введите размеры сторону квадратного окна и количесвто клеток в строке')
+        s = input().split()
+        s, n = int(s[0]), int(s[1])
+        draw_chess_bar(s, n)
     pygame.display.flip()
 
     while pygame.event.wait().type != pygame.QUIT:
